3ef5ec593c86f9feac65d5ab28f8847463ed5dc4
ShareefRahat <imr4h47@gmail.com>
2022-05-19 01:50:22 +0600

    todo strike functionality added

--- src/components/DisplayTodos.js
+++ src/components/DisplayTodos.js
@@ -1,17 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 
 const DisplayTodos = ({ todos, handleDelete }) => {
+  const [complete, setComplete] = useState([]);
+
+  const handleComplete = (title) => {
+    const completed = [...complete, title];
+    console.log(completed);
+    setComplete(completed);
+  };
+
   return (
     <>
       <div className="my-10">
         <h3> Display todos : {todos.length}</h3>
         <section className="grid grid-cols-1 gap-10">
           {todos &&
-            todos.map((todo, index) => (
+            todos.map((todo) => (
               <div className="border border-blue-700 p-5 w-full lg:w-1/2 mx-auto rounded">
-                <p className="text-2xl font-bold">{todo.title}</p>
+                {complete.find((t) => t === todo.title) ? (
+                  <p className="text-2xl font-bold line-through">
+                    {todo.title}
+                  </p>
+                ) : (
+                  <p className="text-2xl font-bold">{todo.title}</p>
+                )}
+
                 <p className="font-serif text-xl">{todo.details}</p>
-                <button className="bg-green-700 px-4 rounded text-white mt-8 mx-8">
+                <button
+                  onClick={() => handleComplete(todo.title)}
+                  className="bg-green-700 px-4 rounded text-white mt-8 mx-8"
+                >
                   Complete
                 </button>
                 <button
